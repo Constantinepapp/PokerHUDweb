7b1dcd38d228c56bef00a043a991a07150e1b06b
Constantinepapp <constantinepapp@outlook.com>
2021-06-24 19:55:52 +0300

    ga try

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
 
+
+const trackingId = "G-65CTVENER1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+})
 ReactDOM.render(
   <React.StrictMode>
     <App />
